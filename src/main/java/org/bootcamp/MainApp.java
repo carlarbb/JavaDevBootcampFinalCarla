e0d7b44ef768e47d689831d39dc12653d6ecdea1
Carla <carla_niculescu@yahoo.com>
2018-04-25 20:17:36 +0300

    Web application

--- src/main/java/org/bootcamp/MainApp.java
+++ src/main/java/org/bootcamp/MainApp.java
@@ -6,52 +6,20 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import sun.applet.Main;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
-public class MainApp  implements CommandLineRunner{
-    private final InsuranceCalculatorService service;
-
-    public MainApp(InsuranceCalculatorService service){
-        this.service= service;
-    }
-    @Override
-    public void run(String... args)throws Exception{
-        final long startTime = System.currentTimeMillis();
-
-            final List<InsuranceCalculationResult> resultList1 = service.calculateAll();
-            final List<InsuranceCalculationResult> resultList2 = service.getCostsHigherThan(1000);
-
-            resultList1.forEach(MainApp::printCalculationResult);
-
-            System.out.println();
-
-            resultList2.forEach(MainApp::printCalculationResult);
-
-            System.out.println();
-
-            printCalculationResult(service.calculateById("3c997def-3cff-11e8-c243-14de190f32bc"));
-
-
-        final long endTime = System.currentTimeMillis();
-
-        System.out.println((endTime - startTime) + " " + TimeUnit.MILLISECONDS.toString());
-    }
-    private static final String OUTPUT_FORMAT = "%s with id %s has total cost %.2f";
+public class MainApp {
 
     public static void main(String[] args) {
         System.out.println("Hello");
         SpringApplication.run(MainApp.class, args);
     }
 
-    private static void printCalculationResult(InsuranceCalculationResult result) {
-
-        if (result != null) {
-            final String output = String.format(OUTPUT_FORMAT, result.getVehicleTypeName(), result.getId(), result.getCost());
-            System.out.println(output);
-        }
-    }
 }
